e19997967d64b8dbd024ed29151f2cd40a6bc651
yonaries <yonaries.dev@gmail.com>
2023-10-13 22:23:37 +0300

    [add]: custom api response added

---
+++ Application/Common/Responses/SuccessResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Application.Responses;
+
+public class SuccessResponse<T>
+{
+    public bool Success { get; set; } = true;
+    public string Message { get; set; }
+    public T Data { get; set; }
+
+    public SuccessResponse(T data)
+    {
+        Data = data;
+    }
+
+    public SuccessResponse(string message, T data)
+    {
+        Message = message;
+        Data = data;
+    }
+}
